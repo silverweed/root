f1b6a21e3ac8088babefb45dee5ec4a7b419e95f
silverweed <giacomo.parolini@cern.ch>
2025-03-31 14:45:18 +0200

    [rfile] add rudimentary key filtering through GetKeys

--- io/v7/rfile/inc/ROOT/RFile.hxx
+++ io/v7/rfile/inc/ROOT/RFile.hxx
@@ -24,11 +24,88 @@
 namespace ROOT {
 namespace Experimental {
 
-class RFile {
+class RFileKeyIterable final {
+   TFile *fFile;
+   std::string fPattern;
+
+public:
+   class RIterator {
+      friend class RFileKeyIterable;
+
+      // NOTE: TFile not present because we already skip it in the ctor (and there is only 1 such key per file)
+      static constexpr const char *fInternalKeyClasses[] = {"KeysList", "StreamerInfo", "FreeSegments"};
+
+      ROOT::Detail::TKeyMapIterable::TIterator fIter;
+      std::string_view fPattern;
+
+      static bool IsInternalKeyClass(const char *className)
+      {
+         if (strlen(className) == 0)
+            return true;
+         for (const char *k : fInternalKeyClasses)
+            if (strcmp(className, k) == 0)
+               return true;
+         return false;
+      }
+
+      bool MatchesPattern(std::string_view name) const {
+         // TODO: more complex pattern handling (glob/regex)
+         if (fPattern.empty())
+            return true;
+
+         if (name.size() < fPattern.size())
+            return false;
+                 
+          return name.compare(0, fPattern.size(), fPattern) == 0;
+      }
+
+      void Advance() {
+         // We only want to return keys that refer to user objects, not internal ones, therefore we skip
+         // all keys that have internal class names.
+         do {
+            ++fIter;
+         } while (fIter.operator->() &&
+                  (IsInternalKeyClass(fIter->fClassName.c_str()) || !MatchesPattern(fIter->fKeyName)));
+      }
+
+      RIterator(ROOT::Detail::TKeyMapIterable::TIterator iter, std::string_view pattern)
+         : fIter(iter), fPattern(pattern)
+      {
+         // Advance the iterator to skip the first key, which is always the TFile key.
+         // Then skip until the first matching key.
+         Advance();
+      }
+
+   public:
+      using iterator = RIterator;
+      using iterator_category = std::forward_iterator_tag;
+      using difference_type = std::ptrdiff_t;
+      using value_type = std::string;
+      using pointer = value_type *;
+      using reference = value_type &;
+
+      iterator operator++()
+      {
+         Advance();
+         return *this;
+      }
+      reference operator*() { return fIter->fKeyName; }
+      pointer operator->() { return &fIter->fKeyName; }
+      bool operator!=(const iterator &rh) const { return !(*this == rh); }
+      bool operator==(const iterator &rh) const { return fIter == rh.fIter; }
+   };
+
+   explicit RFileKeyIterable(TFile *file, std::string_view pattern) : fFile(file), fPattern(pattern) {}
+
+   RIterator begin() const { return {fFile->WalkTKeys().begin(), fPattern}; }
+   RIterator end() const { return {fFile->WalkTKeys().end(), fPattern}; }
+};
+
+class RFile final {
    std::unique_ptr<TFile> fFile;
-   
+
    explicit RFile(std::unique_ptr<TFile> file) : fFile(std::move(file)) {}
-   
+
    // NOTE: these strings are const char * because they need to be passed to TFile
    /// Gets object `path` from the file and returns an **owning** pointer to it.
    /// The caller should immediately wrap it into a unique_ptr of the type described by `type`.
@@ -36,7 +113,7 @@ class RFile {
    /// Writes `obj` to file, without taking its ownership.
    void PutUntyped(const char *path, const TClass *type, void *obj);
 
-public: 
+public:
    ///// Factory methods /////
 
    /// Opens the file for reading
@@ -53,7 +130,8 @@ public:
    // Retrieves an object from the file.
    // If the object is not there, returns an invalid ref.
    template <typename T>
-   std::unique_ptr<T> Get(std::string_view path) const {
+   std::unique_ptr<T> Get(std::string_view path) const
+   {
       std::string pathStr(path);
       const TClass *cls = TClass::GetClass(typeid(T));
       void *obj = GetUntyped(pathStr.c_str(), cls);
@@ -63,13 +141,16 @@ public:
    // Puts an object into the file.
    // Throws a RException if the file was opened in read-only mode.
    template <typename T>
-   void Put(std::string_view path, T &obj) {
+   void Put(std::string_view path, T &obj)
+   {
       std::string pathStr(path);
       const TClass *cls = TClass::GetClass(typeid(T));
       PutUntyped(pathStr.c_str(), cls, &obj);
    }
+
+   RFileKeyIterable GetKeys(std::string_view pattern = "") const { return RFileKeyIterable(fFile.get(), pattern); }
 };
-   
+
 } // namespace Experimental
 } // namespace ROOT
 
